1210df50857dd1a2fba552d93d65827373ce5ed0
João Gabriel Lembo <joaogabriellembo@usp.br>
2023-07-09 23:51:38 -0300

    Adiciona produtos para download

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,45 @@
-import React from "react";
-import HeadComponent from '../components/Head';
+import React, { useEffect, useState } from 'react';
+import Product from "../components/Product";
 
-// Constants
-const TWITTER_HANDLE = "web3dev_";
+import { useWallet } from '@solana/wallet-adapter-react';
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+
+// Constantes
+const TWITTER_HANDLE = 'web3dev_';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
-  
-  
+  const { publicKey } = useWallet();
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    if (publicKey) {
+      fetch(`/api/fetchProducts`)
+        .then(response => response.json())
+        .then(data => {
+          setProducts(data);
+          console.log("Products", data);
+        });
+    }
+  }, [publicKey]);
+
+  const renderNotConnectedContainer = () => (
+    <div className="button-container">
+      <WalletMultiButton className="cta-button connect-wallet-button" />
+    </div>
+  );
+
+
+  const renderItemBuyContainer = () => (
+    <div className="products-container">
+      {products.map((product) => (
+        <Product key={product.id} product={product} />
+      ))}
+    </div>
+  );
+
   return (
     <div className="App">
-      <HeadComponent/>
       <div className="container">
         <header className="header-container">
           <p className="header"> 😳 Loja de emojis 😈</p>
@@ -18,7 +47,7 @@ const App = () => {
         </header>
 
         <main>
-          <img className='gif-image' src="https://media.tenor.com/1mJ-tJSzvwsAAAAd/solana-sol.gif" alt="emoji" />
+          {publicKey ? renderItemBuyContainer() : renderNotConnectedContainer()}
         </main>
 
         <div className="footer-container">
